6e4fb9ff227b57ed9e59b79c13e51f16a20e21f2
Cintya <cintya_milanez@hotmail.com>
2021-04-17 18:48:03 -0300

    Comentario no metodo main.

--- src/main/java/br/com/cadastro/CadastroApplication.java
+++ src/main/java/br/com/cadastro/CadastroApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroApplication {
 
+	// metodo onde o java começa a rodar:
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroApplication.class, args);
 	}
