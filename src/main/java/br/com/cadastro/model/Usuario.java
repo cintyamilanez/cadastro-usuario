90fec0dd823fc23fed1d2e6c4320be5e88ae4836
Cintya <cintya_milanez@hotmail.com>
2021-04-18 18:12:27 -0300

    Criação da entidade Usuário.

---
+++ src/main/java/br/com/cadastro/model/Usuario.java
@@ -0,0 +1,12 @@
+package br.com.cadastro.model;
+
+import java.util.Date;
+
+public class Usuario {
+
+    public String nome;
+    public String email;
+    public String CPF;
+    public Date nascimento;
+
+}
