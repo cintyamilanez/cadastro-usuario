a03e6afdff6b357ba85646417911bf0d49c10a01
Cintya <cintya_milanez@hotmail.com>
2021-04-21 22:17:43 -0300

    Adicionado mapeamentos do spring + projetos postman.

--- src/main/java/br/com/cadastro/CadastroApplication.java
+++ src/main/java/br/com/cadastro/CadastroApplication.java
@@ -1,9 +1,17 @@
 package br.com.cadastro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("br.com.cadastro.service")
+@EnableJpaRepositories("br.com.cadastro.repository")
+@EntityScan("br.com.cadastro.model")
 public class CadastroApplication {
 
 	public static void main(String[] args) {
